9fe4df3fb69dea3bd61b0ddc13042ca3f5c0816c
jglanglois <langlois.jeangil@gmail.com>
2020-05-05 18:08:18 +0200

    Update main.cpp
    update example

--- main.cpp
+++ main.cpp
@@ -7,21 +7,11 @@ using namespace std;
 
 int main()
 {
-    /*
-    // Create player Character
-    string playerName;
-    cout << "Name ?" << endl;
-    getline(cin, playerName);
-    PlayerCharacter player(playerName, 100, 20, 10, 5);
-    */
-
-    // Start a Fight
-    PlayerCharacter player("Toto", 100, 20, 10, 5, 20);
+    PlayerCharacter player("Player", 100, 20, 10, 5, 20);
     Fight fight(player);
     fight.addEnemy("Bat", 10, 0, 10, 0, 30);
     fight.addEnemy("Troll", 20, 0, 20, 5, 5);
     fight.start();
-
 
     return 0;
 }
